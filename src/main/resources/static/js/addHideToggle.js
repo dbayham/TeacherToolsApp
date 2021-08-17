ee3bb6f68ed8ace6d8e175db6129f4877901ff5e
Dustin Bayham <79125833+dbayham@users.noreply.github.com>
2021-08-17 11:51:21 -0400

    Add files via upload

---
+++ src/main/resources/static/js/addHideToggle.js
@@ -0,0 +1,38 @@
+
+function hideShowStudentToClass() {
+    let x = document.getElementById("hideToggleStudentToClass");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+function hideShowAssignmentToClass(){
+    let y = document.getElementById("hideToggleAddNewClass");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "none";
+    }
+  }
+
+  function hideShowNewClassroom(){
+    let z = document.getElementById("hideToggleAddNewClassroom");
+    if (z.style.display === "none") {
+      z.style.display = "block";
+    } else {
+      z.style.display = "none";
+    }
+  }
+
+  function hideShowNewStudent(){
+    let w = document.getElementById("hideToggleNewStudent");
+    if (w.style.display === "none") {
+      w.style.display = "block";
+    } else {
+      w.style.display = "none";
+    }
+  }
+
+ 
